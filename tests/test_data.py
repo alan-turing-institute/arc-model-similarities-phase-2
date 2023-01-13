1ea27a4677c31312ba5d3962aabade6abdb51f7a
philswatton <philswatton@gmail.com>
2023-01-13 14:01:00 +0000

    test for double drop complete non overlap

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,4 +1,5 @@
 from collections import Counter
+from copy import deepcopy
 from math import isclose
 
 from modsim2.data.loader import CIFAR10DataModuleDrop
@@ -34,3 +35,28 @@ def test_drop_loader_stratification():
         for i in range(9)
     ]
     assert all(count_test)
+
+
+def test_drop_loader_double_overlap():
+    # Generate datasets A and B. Drop 10% from both with no overlap
+    drop = 0.1
+    cifar_A = CIFAR10DataModuleDrop(drop=drop)
+    cifar_B = deepcopy(cifar_A)
+    cifar_B.keep = "B"
+
+    # Set up the datasets
+    cifar_A.prepare_data()
+    cifar_A.setup()
+    cifar_B.prepare_data()
+    cifar_B.setup()
+    dl_A = cifar_A.train_dataloader()
+    dl_B = cifar_B.train_dataloader()
+
+    # Extract indices from both datasets to work as lists
+    indices_A = set(dl_A.dataset.indices)
+    indices_B = set(dl_B.dataset.indices)
+
+    # Since no overlap in data dropped, if we drop x obs, both shld contain
+    # N-x obs, with x obs not in the other. Shared obs should therefore be equal
+    # to N-2x
+    assert len(indices_A & indices_B) == len(dl_A.dataset.dataset) * (1 - drop * 2)
