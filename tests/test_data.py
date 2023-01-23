61529da9be8c78d5ae6da705dd517ebeca818d98
philswatton <philswatton@gmail.com>
2023-01-23 18:08:49 +0000

    testing A and B in functions, testing batch size added

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,23 +1,32 @@
 from collections import Counter
-from math import isclose
+from math import ceil, isclose
 
 from modsim2.data.loader import DMPair
 
 
-def test_cifar_pair_n_obs():
-    drop = 0.1
-    dmpair = DMPair(drop_percent_A=drop, drop_percent_B=0)
-    assert len(set(dmpair.indices_A)) == (1 - drop) * len(set(dmpair.indices_B))
+def _test_dm_n_obs(
+    length_subset: int,
+    length_full: int,
+    drop: float,
+) -> None:
+    assert length_subset == (1 - drop) * length_full
 
 
-def test_cifar_pair_stratification():
+def test_cifar_A_n_obs():
     drop = 0.1
     dmpair = DMPair(drop_percent_A=drop)
+    _test_dm_n_obs(len(dmpair.A.dataset_train), len(dmpair.cifar.dataset_train), drop)
 
-    # Extract labels from full and subsetted dataset
-    full_labels = [i[1] for i in dmpair.A.dataset_train.dataset]
-    subset_labels = dmpair.labels_A
 
+def test_cifar_B_n_obs():
+    drop = 0.1
+    dmpair = DMPair(drop_percent_B=drop)
+    _test_dm_n_obs(len(dmpair.B.dataset_train), len(dmpair.cifar.dataset_train), drop)
+
+
+def _test_dm_stratification(
+    full_labels: list[int], subset_labels: list[int], drop: float
+) -> None:
     # Get count of labels in each dataset
     full_count = Counter(full_labels)
     subset_count = Counter(subset_labels)
@@ -30,17 +39,80 @@ def test_cifar_pair_stratification():
     assert all(count_test)
 
 
-def test_cifar_pair_overlap():
-    # Generate datasets A and B. Drop 10% from both with no overlap
+def test_cifar_A_stratification():
     drop = 0.1
     dmpair = DMPair(drop_percent_A=drop, drop_percent_B=drop)
-    dl_A = dmpair.A.train_dataloader()
+    full_labels = [image[1] for image in dmpair.cifar.dataset_train]
+    _test_dm_stratification(full_labels, dmpair.labels_A, drop=drop)
 
+
+def test_cifar_B_stratification():
+    drop = 0.1
+    dmpair = DMPair(drop_percent_A=drop, drop_percent_B=drop)
+    full_labels = [image[1] for image in dmpair.cifar.dataset_train]
+    _test_dm_stratification(full_labels, dmpair.labels_B, drop=drop)
+
+
+def _test_dm_overlap(
+    indices_A: list[int],
+    indices_B: list[int],
+    drop_percentage_A: float,
+    drop_percentage_B: float,
+    original_size: int,
+) -> None:
     # Get indices, put into set
-    indices_A = set(dmpair.indices_A)
-    indices_B = set(dmpair.indices_B)
+    indices_A = set(indices_A)
+    indices_B = set(indices_B)
 
-    # Since no overlap in data dropped, if we drop x obs, both shld contain
-    # N-x obs, with x obs not in the other. Shared obs should therefore be equal
-    # to N-2x
-    assert len(indices_A & indices_B) == len(dl_A.dataset.dataset) * (1 - drop * 2)
+    # Below is based on notion that there should be no overlap in data
+    # dropped from both datasets
+    total_drop_percentage = drop_percentage_A + drop_percentage_B
+    shared_size = original_size * (1 - total_drop_percentage)
+    assert len(indices_A & indices_B) == shared_size
+
+
+def test_cifar_pair_overlap_same_size():
+    drop = 0.1
+    dmpair = DMPair(drop_percent_A=drop, drop_percent_B=drop)
+    original_size = len(dmpair.cifar.dataset_train)
+    _test_dm_overlap(
+        dmpair.indices_A,
+        dmpair.indices_B,
+        dmpair.drop_percent_A,
+        dmpair.drop_percent_B,
+        original_size,
+    )
+
+
+def test_cifar_pair_overlap_diff_size():
+    dmpair = DMPair(drop_percent_A=0.1, drop_percent_B=0.2)
+    original_size = len(dmpair.cifar.dataset_train)
+    _test_dm_overlap(
+        dmpair.indices_A,
+        dmpair.indices_B,
+        dmpair.drop_percent_A,
+        dmpair.drop_percent_B,
+        original_size,
+    )
+
+
+def _test_cifar_dataloader_batch_count(data_loader, batch_size):
+    dataset_size = len(data_loader.dataset)
+    count = 0
+    for batch in data_loader:
+        count += 1
+    assert count == ceil(dataset_size / batch_size)
+
+
+def test_cifar_A_batch_count():
+    batch_size = 32
+    dmpair = DMPair(drop_percent_A=0.175, batch_size=batch_size)
+    dla = dmpair.A.train_dataloader()
+    _test_cifar_dataloader_batch_count(dla, batch_size)
+
+
+def test_cifar_B_batch_count():
+    batch_size = 32
+    dmpair = DMPair(drop_percent_B=0.175, batch_size=batch_size)
+    dla = dmpair.B.train_dataloader()
+    _test_cifar_dataloader_batch_count(dla, batch_size)
