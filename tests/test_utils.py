b17db04f96435ac6d7a20737cd1a5acf0d943cf6
philswatton <philswatton@gmail.com>
2023-02-07 13:06:28 +0000

    init commit

---
+++ tests/test_utils.py
@@ -0,0 +1,21 @@
+import os
+
+from modsim2.utils.config import load_configs
+
+# project root = arc-model-similarites-phase-2/
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+
+# Get configs
+METRICS_CONFIG_PATH = os.path.join(PROJECT_ROOT, "tests", "testconfig", "metrics.yaml")
+CONFIGS = load_configs(METRICS_CONFIG_PATH)
+
+
+def test_configs_exist():
+    assert "metric_config" in CONFIGS
+
+
+def test_metric_configs_exist():
+    metric_config = CONFIGS["metric_config"]
+    assert "mmd_rbf" in metric_config
+    assert "function" in metric_config["mmd_rbf"]
+    assert "arguments" in metric_config["mmd_rbf"]
