a3d0dd40ec10ffa7305129b809538fb4264ff2ed
philswatton <philswatton@gmail.com>
2023-02-09 11:03:26 +0000

    further clarified arr2mat

--- src/modsim2/similarity/embeddings.py
+++ src/modsim2/similarity/embeddings.py
@@ -4,7 +4,7 @@ import numpy as np
 def array_to_matrix(array: np.ndarray) -> np.ndarray:
     """
     Reshapes np.ndarrays of >=2 dimensions to a 2-dimensional
-    np.ndarray
+    np.ndarray, while preserving the first dimension
 
     Args:
         array (np.ndarray): An np.ndarray with observations on
