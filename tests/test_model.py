bf3ee5e31038bd5e747b41380469b1b8c621bd06
philswatton <philswatton@gmail.com>
2023-02-16 09:24:22 +0000

    Moved trainer setup inside test

--- tests/test_model.py
+++ tests/test_model.py
@@ -7,22 +7,23 @@ from pytorch_lightning.loggers import CSVLogger
 from modsim2.data.loader import DMPair
 from modsim2.model.resnet import ResnetModel
 
-# Constants for testing model running and training
-dmpair = DMPair(val_split=testing_constants.VAL_SPLIT)
-model = ResnetModel(lr=0.05)
-trainer = Trainer(
-    max_epochs=1,
-    max_steps=2,
-    devices=None,
-    logger=CSVLogger(save_dir="logs/test_logs/"),
-    callbacks=[
-        LearningRateMonitor(logging_interval="step"),
-        TQDMProgressBar(refresh_rate=10),
-    ],
-)
-
 
 def test_model_runs():
+    # Set up trainer
+    dmpair = DMPair(val_split=testing_constants.VAL_SPLIT)
+    model = ResnetModel(lr=0.05)
+    trainer = Trainer(
+        max_epochs=1,
+        max_steps=2,
+        devices=None,
+        logger=CSVLogger(save_dir="logs/test_logs/"),
+        callbacks=[
+            LearningRateMonitor(logging_interval="step"),
+            TQDMProgressBar(refresh_rate=10),
+        ],
+    )
+
+    # Make sure that it runs
     try:
         trainer.fit(model, dmpair.A)
     except Exception as exc:
