eeb9e99da80e5785c06df266dc7700c140fd7a05
philswatton <philswatton@gmail.com>
2023-02-23 14:33:28 +0000

    compute metrics for a dmpair

--- scripts/calculate_metrics.py
+++ scripts/calculate_metrics.py
@@ -1,18 +1,9 @@
 import argparse
 
 import yaml
+from functions import opts2dmpairArgs
 
 from modsim2.data.loader import DMPair
-
-
-def opts2dmpairArgs(opt: dict, seed: int) -> dict:
-    return {
-        "drop_percent_A": opt["A"]["drop"],
-        "drop_percent_B": opt["B"]["drop"],
-        "transforms_A": opt["A"]["transforms"],
-        "transforms_B": opt["B"]["transforms"],
-        "seed": seed,
-    }
 
 
 def main(
