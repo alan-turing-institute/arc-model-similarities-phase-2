c185ae35c90a3544d60cd3a578382ee71a91815e
philswatton <philswatton@gmail.com>
2023-03-20 14:17:14 +0000

    load_model returns the run instead of finishing it

--- src/modsim2/model/load_model.py
+++ src/modsim2/model/load_model.py
@@ -8,15 +8,27 @@ from modsim2.model.resnet import ResnetModel
 
 
 def download_model(
-    model_name: str,
-    folder: Optional[str] = "turing-arc/ms2/",  # TODO: argparse in script
+    experiment_name: str,
+    entity="turing-arc",
+    project_name="ms2",
     version: Optional[str] = ":latest",  # TODO: argparse in script
 ) -> ResnetModel:
+    # Names to use in restoring the model
+    model_name = experiment_name + "_model"
+    folder = entity + "/" + project_name + "/"
+    experiment_id = experiment_name + "_test"  # TODO: change this to a config
+
     # Download the model
-    run = wandb.init()
+    run = wandb.init(
+        project=project_name,
+        entity=entity,
+        name=experiment_name,
+        id=experiment_id,
+        resume=True,
+    )
     artifact = run.use_artifact(folder + model_name + version, type="model")
     artifact_dir = artifact.download()
-    run.finish()
+    # run.finish()
 
     # Read in the model
     path = os.path.join(artifact_dir, "model.ckpt")
@@ -24,4 +36,4 @@ def download_model(
     model = ResnetModel()
     model.load_state_dict(checkpoint["state_dict"])
 
-    return model
+    return model, run
