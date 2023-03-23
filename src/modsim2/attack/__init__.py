c4b2b086913f1536e33d077d464012a0d23a04da
philswatton <philswatton@gmail.com>
2023-03-23 09:40:25 +0000

    file rename

--- src/modsim2/attack/__init__.py
+++ src/modsim2/attack/__init__.py
@@ -1,2 +1,2 @@
-from modsim2.attack.attack import generate_over_combinations  # noqa: F401
+from modsim2.attack.adversarial_images import generate_over_combinations  # noqa: F401
 from modsim2.attack.transfer import compute_transfer_attack  # noqa: F401
