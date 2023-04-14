d94bed5c6087753f8e3f50689d4f6c3421df290f
joannacknight <jknight@turing.ac.uk>
2023-04-14 08:26:28 +0100

    update poetry setup to include otdd dependencies

--- src/modsim2/similarity/ot.py
+++ src/modsim2/similarity/ot.py
@@ -1,8 +1,20 @@
 import logging
 
+import numpy as np
+
 # Set module logger
 logger = logging.getLogger(__name__)
 
 
-def ot() -> float:
+def ot(array_A: np.ndarray, array_B: np.ndarray) -> float:
+    """
+    Description to do
+
+    Args:
+        array_A: The first image dataset
+        array_B: The second image dataset
+
+    Returns:
+        float: The ot between A and B
+    """
     pass
