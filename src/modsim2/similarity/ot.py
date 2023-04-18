7f43993d5922d29c362ea3ca9f82bbebaef2c50a
joannacknight <jknight@turing.ac.uk>
2023-04-18 13:55:04 +0100

    implemented vanilla otdd example

--- src/modsim2/similarity/ot.py
+++ src/modsim2/similarity/ot.py
@@ -1,8 +1,36 @@
 import logging
 
+import numpy as np
+from otdd.pytorch.distance import DatasetDistance
+
 # Set module logger
 logger = logging.getLogger(__name__)
 
 
-def ot() -> float:
-    pass
+def ot(
+    array_A: np.ndarray,
+    array_B: np.ndarray,
+    implementation: str = "otdd",
+    max_samples: int = 10,
+    **kwargs,
+) -> float:
+    """
+    Calculates the optimal transport distance between datasets
+
+    Args:
+        array_A: The first image dataset
+        array_B: The second image dataset
+        implementation (str): the implementation type of OT, currently only 'otdd'
+                              is accepted
+        max_samples (int):  maximum number of samples used in outer-level otdd problem.
+
+
+    Returns:
+        float: The ot between A and B
+    """
+    if implementation == "otdd":
+        dist = DatasetDistance(array_A, array_B, **kwargs)
+        d = dist.distance(maxsamples=max_samples)
+        return d
+    else:
+        raise ValueError("Unknown implementation:" + implementation)
---
+++ tests/test_ot.py
@@ -0,0 +1,22 @@
+import os
+
+from modsim2.data.loader import DMPair
+from modsim2.utils.config import load_configs
+
+# project root = arc-model-similarites-phase-2/
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+
+# Get metric config
+METRICS_CONFIG_PATH = os.path.join(PROJECT_ROOT, "tests", "testconfig", "metrics.yaml")
+CONFIGS = load_configs(METRICS_CONFIG_PATH)
+METRIC_CONFIG = CONFIGS["metric_config"]
+METRIC_CONFIG = {k: v for k, v in METRIC_CONFIG.items() if v["function"] == "ot"}
+
+
+def test_cifar_ot_same():
+    dmpair = DMPair(metric_config=METRIC_CONFIG)
+
+    similarity_dict = dmpair.compute_similarity(only_train=True, return_dataset=True)
+    for k in similarity_dict:
+        print(k + " similarity distance:", float(similarity_dict[k]))
+        assert similarity_dict[k] == 0
