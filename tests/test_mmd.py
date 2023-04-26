a844ca2ae87b05b1c242ab522e446c91bade427e
joannacknight <jknight@turing.ac.uk>
2023-04-26 09:54:04 +0100

    update to pytest fixtures in metrics tests

--- tests/test_mmd.py
+++ tests/test_mmd.py
@@ -5,15 +5,15 @@ import pytest
 from modsim2.data.loader import DMPair
 from modsim2.utils.config import load_configs
 
-# project root = arc-model-similarites-phase-2/
-PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
-METRICS_CONFIG_PATH = os.path.join(PROJECT_ROOT, "tests", "testconfig", "metrics.yaml")
-
 
 # Fixture that returns the metric config dictionary
 @pytest.fixture(scope="module")
 def metrics_config() -> dict:
-    mmd_config = load_configs(METRICS_CONFIG_PATH)["metrics_config"]
+    project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+    metrics_config_path = os.path.join(
+        project_root, "tests", "testconfig", "metrics.yaml"
+    )
+    mmd_config = load_configs(metrics_config_path)["metrics_config"]
     # filter down to only mmd configs
     mmd_config = {k: v for k, v in mmd_config.items() if v["class"] == "mmd"}
     return mmd_config
@@ -21,7 +21,7 @@ def metrics_config() -> dict:
 
 # This test checks that the distance between a dataset and itself is the expect value
 # For this metric the expected value is always zero
-def test_cifar_mmd_same(metrics_config: pytest.fixture):
+def test_cifar_mmd_same(metrics_config: dict):
     dmpair = DMPair(metrics_config=metrics_config)
     similarity_dict = dmpair.compute_similarity()
     similarity_dict_only_train = dmpair.compute_similarity(only_train=True)
@@ -34,7 +34,7 @@ def test_cifar_mmd_same(metrics_config: pytest.fixture):
 # value
 # The calculated distance is checked against the known value for the seed - brittle
 # test but useful for messing with code
-def test_cifar_mmd_different(metrics_config: pytest.fixture):
+def test_cifar_mmd_different(metrics_config: dict):
     dmpair = DMPair(metrics_config=metrics_config, drop_percent_A=0.2, seed=42)
     similarity_dict = dmpair.compute_similarity()
     similarity_dict_only_train = dmpair.compute_similarity(only_train=True)
--- tests/test_otdd.py
+++ tests/test_otdd.py
@@ -5,15 +5,15 @@ import pytest
 from modsim2.data.loader import DMPair
 from modsim2.utils.config import load_configs
 
-# project root = arc-model-similarites-phase-2/
-PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
-METRICS_CONFIG_PATH = os.path.join(PROJECT_ROOT, "tests", "testconfig", "metrics.yaml")
-
 
 # Fixture that returns the metric config dictionary
 @pytest.fixture(scope="module")
 def metrics_config() -> dict:
-    otdd_config = load_configs(METRICS_CONFIG_PATH)["metrics_config"]
+    project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+    metrics_config_path = os.path.join(
+        project_root, "tests", "testconfig", "metrics.yaml"
+    )
+    otdd_config = load_configs(metrics_config_path)["metrics_config"]
     # filter down to only otdd configs
     otdd_config = {k: v for k, v in otdd_config.items() if v["class"] == "otdd"}
     return otdd_config
@@ -23,7 +23,7 @@ def metrics_config() -> dict:
 # When exact calculations are used then this will be zero
 # Approximate methods may be non-zero and these are checked against the known value
 # for the seed
-def test_cifar_otdd_same(metrics_config: pytest.fixture):
+def test_cifar_otdd_same(metrics_config: dict):
     dmpair = DMPair(metrics_config=metrics_config, seed=42)
     similarity_dict = dmpair.compute_similarity(only_train=False)
     similarity_dict_only_train = dmpair.compute_similarity(only_train=True)
@@ -41,7 +41,7 @@ def test_cifar_otdd_same(metrics_config: pytest.fixture):
 # value
 # The calculated distance is checked against the known value for the seed - brittle
 # test but useful for messing with code
-def test_cifar_otdd_different(metrics_config: pytest.fixture):
+def test_cifar_otdd_different(metrics_config: dict):
     dmpair = DMPair(metrics_config=metrics_config, drop_percent_A=0.2, seed=42)
     similarity_dict = dmpair.compute_similarity(only_train=False)
     similarity_dict_only_train = dmpair.compute_similarity(only_train=True)
