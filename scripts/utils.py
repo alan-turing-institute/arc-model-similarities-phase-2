26761bf8da4bcfc8d73ba25be5d9483046b5a65d
philswatton <philswatton@gmail.com>
2023-05-04 16:52:13 +0100

    now composes transforms if needed

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,8 +1,11 @@
+from modsim2.utils.config import compose_transform
+
+
 def opts2dmpairArgs(opt: dict, seed: int) -> dict:
     return {
         "drop_percent_A": opt["A"]["drop"],
         "drop_percent_B": opt["B"]["drop"],
-        "transforms_A": opt["A"]["transforms"],
-        "transforms_B": opt["B"]["transforms"],
+        "transforms_A": compose_transform(opt["A"]["transforms"]),
+        "transforms_B": compose_transform(opt["B"]["transforms"]),
         "seed": seed,
     }
