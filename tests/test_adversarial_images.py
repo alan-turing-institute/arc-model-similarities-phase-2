d96805f82c1bc1b2ec6e2221d8d61e1373f0d1b1
philswatton <philswatton@gmail.com>
2023-07-13 13:40:03 +0100

    updated adversarial image test

--- tests/test_adversarial_images.py
+++ tests/test_adversarial_images.py
@@ -26,7 +26,9 @@ def test_best_attack_selection():
 
     # this means the best selection function should *always* select the
     # second image in the list (as this corresponds to the lowest epsilon 0.3
-    # that is still success)
-    best_images = select_best_attack(images=images, success=success, epsilons=epsilons)
+    # that is still successful)
+    best_images, _ = select_best_attack(
+        images=images, success=success, epsilons=epsilons
+    )
     assert_equal = functools.partial(torch.testing.assert_close, rtol=0, atol=0)
     assert_equal(images[1], best_images)
